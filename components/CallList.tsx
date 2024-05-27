dde9771cf2bf5d824aae912ffd8639a87ea5fc28
engr-okoroafor <engr.okoroafor@gmail.com>
2024-05-27 15:38:29 +0100

    changed semicolon to backtick

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -100,7 +100,7 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
             link={
               type === "recordings"
                 ? meeting.url
-                : "${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meeting.id}"
+                : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meeting.id}`
             }
           />
         ))
